eaea37cb657fca43286aad418997e9ec2d019263
Vamora <aniserri.vamora@gmail.com>
2026-06-19 15:43:06 +0100

    Fix formatting of main component in page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-<main className={styles.main}>
-  <h1>Hello world!</h1> 
-  <p>Vamora Weather is under development</p> 
-</main>
+    <main className={styles.main}>
+      <h1>Hello world!</h1>
+      <p>Vamora Weather is under development</p>
+    </main>
   );
 }
