a49acc94f67f568ea245830e8cada3168fcdf0bf
Vamora <aniserri.vamora@gmail.com>
2026-06-19 15:41:29 +0100

    Update Home component with development message
    Added a heading and a paragraph to the main content.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-
+<main className={styles.main}>
+  <h1>Hello world!</h1> 
+  <p>Vamora Weather is under development</p> 
+</main>
   );
 }
