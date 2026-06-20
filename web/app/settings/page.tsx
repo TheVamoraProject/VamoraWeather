c63e45f3e6c3412b2dce6649692a4602d4800dd9
Vamora <aniserri.vamora@gmail.com>
2026-06-20 11:55:39 +0100

    Create page.tsx

---
+++ web/app/settings/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Toolbar from "@/components/Toolbar";
+import BottomNavbar from "@/components/BottomNavbar";
+import { Home, Settings } from "lucide-react";
+
+export default function Page() {
+  return (
+  <>
+    <Toolbar title="Settings" />
+    <BottomNavbar
+      items={[
+        { id: "home", href: "/", icon: "home", label: "Home" },
+        { id: "settings", href: "/settings", icon: "settings", label: "Settings" },
+      ]}
+    />
+  </>
+);
+}
