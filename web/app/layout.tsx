8315404f8f445b9c0a1e7d026169a5c0911909f7
Vamora <aniserri.vamora@gmail.com>
2026-06-28 22:12:26 +0100

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import RegisterSW from "./register-sw";
 
 export const metadata = {
   title: "Weather",
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <RegisterSW />
         {children}
       </body>
     </html>
